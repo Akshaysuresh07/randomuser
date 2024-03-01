5719c8d6f71e8614cde8e3c0e8d399381bc3caab
Akshaysuresh07 <akshaysureshm010@gmail.com>
2024-03-01 13:49:09 +0530

    random user

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-
-import './App.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [user,setUser]=useState([])
-  const [bgChange,setBgChange]=useState(true)
+  const [user, setUser] = useState([]);
+  const [bgChange, setBgChange] = useState(true);
+  const [companyInfo, setCompanyInfo] = useState({});
+  const [address, setAddress] = useState({});
 
-  useEffect(()=>{
-    fetchapi()
-   
-  },[])
+  useEffect(() => {
+    fetchapi();
+  }, []);
 
-
- const getRandomColor = () => {
+  const getRandomColor = () => {
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
@@ -22,57 +21,52 @@ function App() {
     return color;
   };
 
-  const fetchapi=async()=>{
-    const response = await axios.get('https://dummyjson.com/users')
+  const fetchapi = async () => {
+    const response = await axios.get('https://dummyjson.com/users');
+    const randomIndex = Math.floor(Math.random() * response.data.users.length);
+    const randomUser = response.data.users[randomIndex];
+    setUser(randomUser);
+    setBgChange(getRandomColor());
+    setCompanyInfo({
+      title: randomUser.company.title,
+      name: randomUser.company.name
+    });
+    setAddress({
+      city: randomUser.address.city,
+      state: randomUser.address.state,
+      street: randomUser.address.street
+    });
 
-    const randomindex=Math.floor(Math.random()*response.data.users.length)
-    console.log(randomindex)
-    console.log(response.data.users[randomindex].company);
-    setUser(response.data.users[randomindex])
-    setBgChange(getRandomColor())
+  };
 
-  }
-  const handleRefresh=async()=>{
-    const res=await fetchapi()
-    console.log(res);
-    const newBgColor = getRandomColor();
-    setBgChange(newBgColor);
-
-  }
-
-
+  const handleRefresh = async () => {
+    await fetchapi();
+    setBgChange(getRandomColor());
+  };
 
   return (
-    
-     <>
+    <>
       <h1 className='text-center text-fw-3'>Random User</h1>
-     
-     <div className=' mt-3 item-center  ' style={{ backgroundColor:bgChange,marginLeft:"400px", padding: 20 ,width:"700px" }}>
-    <div className='d-flex me-5 justify-content-center '>
-      
-       <img src={user.image} alt="User profile" />
-      <div className='p-5' >
-         <p>Name: {user.firstName} {user.lastName}</p>
-         <p>Email: {user.email}</p>
-         <p>Phone: {user.phone}</p>
-          <p>Age: {user.age}</p>
+      <div className='mt-3  border-rounded' style={{ backgroundColor: bgChange, marginLeft: "200px", padding: 20, width: "1000px" }}>
+        <div className='d-flex me-5 justify-content-center'>
+          <img src={user.image} alt="User profile" />
+          <div className='p-5'>
+            <p>Name: {user.firstName} {user.lastName}</p>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
+            <p>Address: {address.street}, {address.city}, {address.state}</p>
+            <p>Age: {user.age}</p>
+            <p>Company Name: {companyInfo.name}</p>
+            <p>Job Title : {companyInfo.title}</p>
           
-   
-
-          
+          </div>
+        </div>
+        <div className='d-flex justify-content-center'>
+          <button style={{ backgroundColor: "green" }} onClick={handleRefresh}>Refresh</button>
+        </div>
       </div>
-   
-      
-     </div>
-    <div className='d-flex justify-content-center'>
-     <button style={{backgroundColor:"green"}}  onClick={handleRefresh}>Refresh</button>
-     </div>
-    </div>
-  
-     </>
-    
-    
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
